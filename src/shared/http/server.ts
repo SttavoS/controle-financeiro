315fb94fcbd0632d8bf5a770b229e1b2edfd4616
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-10-19 01:08:25 -0300

    chore: add typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config';
+import 'reflect-metadata';
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import applicationError from './middlewares/application.error';
+import '@/shared/typeorm';
 
 const server = express();
 server.use(cors());
