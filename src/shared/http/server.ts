16b01f9b45e166c97705c0b8c06f2512b85e1d0b
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-10-23 02:00:29 -0300

    fix: add express-async-errors to server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import 'express-async-errors';
 import applicationError from './middlewares/application.error';
 
 const server = express();
