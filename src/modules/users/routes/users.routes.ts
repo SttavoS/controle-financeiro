b2832081a497d3ae69c37aac6c2730ca31dd140c
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-10-28 17:17:17 -0300

    feat(users): add routes

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import UsersController from '../controllers/UsersController';
+
+const usersRoutes = Router();
+const usersController = new UsersController();
+
+usersRoutes.post('/', usersController.create);
+usersRoutes.get('/:id', usersController.show);
+
+export default usersRoutes;
