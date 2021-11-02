58e3dcc9563393982866d99bb46b0c83ba88a505
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-11-02 02:36:53 -0300

    feat(users): add login route

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,10 +1,15 @@
+import authenticated from '@/shared/http/middlewares/authenticated';
 import { Router } from 'express';
+import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
 
 const usersRoutes = Router();
 const usersController = new UsersController();
+const authController = new AuthController();
 
 usersRoutes.post('/', usersController.create);
-usersRoutes.get('/:id', usersController.show);
+usersRoutes.get('/:id', authenticated, usersController.show);
+
+usersRoutes.post('/tokens', authController.login);
 
 export default usersRoutes;
