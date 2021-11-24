f98a591fc1775085628d073b6c4844652086c513
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-11-24 15:03:39 -0300

    fix(wallets): add user attribute to wallet entity

--- src/modules/wallets/entities/Wallet.ts
+++ src/modules/wallets/entities/Wallet.ts
@@ -1,11 +1,15 @@
+import User from '@/modules/users/entities/User';
+
 export default class Wallet {
   name: string;
   private balance: number;
   type: string;
+  user: User;
 
-  constructor(name: string, amount: number, type: string) {
+  constructor(name: string, amount: number, type: string, user: User) {
     this.name = name.trim();
     this.balance = amount;
     this.type = type.trim();
+    this.user = user;
   }
 }
